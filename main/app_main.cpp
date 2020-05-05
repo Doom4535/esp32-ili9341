ee2ff654da58f56f1e78df51c77f05da73c04764
Aaron Covrig <aarcov@gmail.com>
2020-05-05 16:17:31 -0400

    Modified demo and added more info on heltec board

--- main/app_main.cpp
+++ main/app_main.cpp
@@ -22,9 +22,8 @@ extern "C" void app_main(void)
 
     ssd1306_setFixedFont(ssd1306xled_font6x8);
     
-    ssd1306_128x64_i2c_initEx(I2C_SCL_PIN_NUM, I2C_SDA_PIN_NUM, 0);
+    ssd1306_128x64_i2c_initEx(I2C_SCL_PIN_NUM, I2C_SDA_PIN_NUM, 0x3C);
     
-    //ili9341_setRotation(1);
     ssd1306_clearScreen();
 
     ssd1306_negativeMode();
@@ -32,12 +31,23 @@ extern "C" void app_main(void)
     ssd1306_printFixed(0, 110, "Inverted bold", STYLE_BOLD);
     ssd1306_positiveMode();
     
-    int x;
     char buffer[100];
-    for ( x = 0; x < 8; x++ ) {
-	sprintf(buffer, "Testing %d\n", x);
-        ssd1306_print(buffer);
+    ssd1306_print( "Demo with Lexus2K's:\n" );
+    ssd1306_print( "ssd1306 display lib\n");
+    ssd1306_print("Testing...\n");
+    for ( int i = 0; i < 26; i++ ) {
+        sprintf(buffer, ".");
+	if ( !( i % 3 ) ) {
+            sprintf(buffer, "%d", i);
+	}
+	else {
+            sprintf(buffer, ".");
+	}
+	ssd1306_print(buffer);
+	vTaskDelay( pdMS_TO_TICKS( 500 ) );
     }
+    ssd1306_print("\n");
+    ssd1306_print("Demo Complete");
 
     vTaskDelay(30 / portTICK_PERIOD_MS);
 }
